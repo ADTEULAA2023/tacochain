f665a5a9593d48dd6efa6a6d12ad171c72c3ffd9
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-05-11 19:10:48 -0400

    Added folder creation if not exists at the start of the program

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/ADTEULAA2023/tacochain/cmd"
 )
 
 func main() {
+	err := os.Mkdir("./tmp", os.ModePerm)
+	if err != nil {
+		log.Panicln(err)
+	}
+
 	cmd.Execute()
 }
